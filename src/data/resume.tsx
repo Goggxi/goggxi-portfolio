8d2ee8dfabeed6d7e98d711a153c7c7e7ad37ad7
goggxi <goggxi@gmail.com>
2024-08-07 17:48:36 +0700

    initial commit for template

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,61 +2,56 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
+  name: "Rifkan",
   initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  url: "https://goggxi.github.io",
+  location: "Tangerang Indonesia, Indonesia",
+  locationLink: "https://maps.app.goo.gl/SyHoAyNgo591pN2Q7",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "'Kang Ketik Intelek' I am a software engineer passionate about exploring the latest advancements in technology. Always eager to learn and innovate, I thrive on discovering and mastering cutting-edge tools and techniques to deliver impactful and efficient solutions.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
   skills: [
-    "React",
-    "Next.js",
-    "Typescript",
-    "Node.js",
     "Python",
     "Go",
-    "Postgres",
-    "Docker",
-    "Kubernetes",
+    "Dart",
+    "Node.js",
+    "Kotlin",
     "Java",
-    "C++",
+    "Postgres",
+    "MongoDB",
+    "Docker",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
-    email: "hello@example.com",
-    tel: "+123456789",
+    email: "mail@mail.com",
+    tel: "+1234567890",
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://dub.sh/dillion-github",
+        url: "https://github.com/Goggxi",
         icon: Icons.github,
-
         navbar: true,
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://dub.sh/dillion-linkedin",
+        url: "https://linkedin.com/in/goggxi",
         icon: Icons.linkedin,
-
         navbar: true,
       },
       X: {
         name: "X",
-        url: "https://dub.sh/dillion-twitter",
+        url: "https://x.com/moh_rifkan",
         icon: Icons.x,
-
         navbar: true,
       },
       Youtube: {
         name: "Youtube",
-        url: "https://dub.sh/dillion-youtube",
+        url: "https://www.youtube.com/@moh_rifkan",
         icon: Icons.youtube,
         navbar: true,
       },
@@ -64,7 +59,6 @@ export const DATA = {
         name: "Send Email",
         url: "#",
         icon: Icons.email,
-
         navbar: false,
       },
     },
